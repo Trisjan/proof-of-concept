4d21dc8c92fb1a5b9f3303ed94010f4d8d9d9811
Trisjan <TrisjanM@hotmail.com>
2023-06-12 01:39:00 +0200

    First opzetje

---
+++ index.js
@@ -0,0 +1,34 @@
+// Importeer express uit de node_modules map
+import express, { response } from "express";
+
+// Maak een nieuwe express app
+const app = express();
+
+// Stel in hoe we express gebruiken
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+    response.render("index");
+})
+
+// Stel het poortnummer in waar express op gaat luisteren
+app.set("port", process.env.PORT || 8000);
+
+// Start express op, haal het ingestelde poortnummer op
+app.listen(app.get("port"), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+    return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
